77a65209b389f51844833fd65f25bed727896d1a
Felipe Trova <felipee.trova@gmail.com>
2020-05-21 12:35:55 -0300

    Removed file not used + init list shoes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Provider } from 'react-redux'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
+import ShoesList from './components/Shoes/Shoes';
 
-      </div>
-    )
-  }
-}
+import store from './store'
+
+const App = () => <Provider store={store}><ShoesList /></Provider>;
 
 export default App;
